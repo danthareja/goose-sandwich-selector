7d13ae0669030fb1c05098b72359d6f7d270a313
Dan Thareja <danthareja@gmail.com>
2014-08-25 12:18:25 -0700

    adds sandwich ingredient seleciton to main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,9 +2,50 @@
 
 angular.module('gooseSandwichApp')
   .controller('MainCtrl', function ($scope, Tweet) {
+    $scope.meatList = ['bacon',
+                    'buffalo chicken',
+                    'cheesesteak',
+                    'chicken',
+                    'corned beef',
+                    'eggs',
+                    'grilled chicken',
+                    'ham',
+                    'meatballs',
+                    'pepper ham',
+                    'pepperoni',
+                    'prosciutto',
+                    'pepper turkey',
+                    'roast beef',
+                    'santa fe turkey',
+                    'salami',
+                    'sausage',
+                    'turkey',];
+
+    $scope.cheeseList = ['any',
+                      'american',
+                      'cheddar',
+                      'cheddar jack',
+                      'muenster',
+                      'pepper jack',
+                      'provolone',
+                      'swiss'];
+
+    $scope.sauceList = ['none',
+                        'au jus',
+                        'barbecue',
+                        'hot sauce',
+                        'mayo',
+                        'mustard',
+                        'ranch',
+                        'russian',
+                        'tomato sauce'];
+
+    $scope.meatRadio = {model: undefined};
+    $scope.cheeseRadio = {model: 'any'};
+    $scope.sauceRadio = {model: 'none'};
+
 
     $scope.orderSandwich = function() {
-
       // Maybe add some random tony compliment at the end? or Tony hashtag?
       var order = { tweet: "(at) thegoosemen the I just ordered a " + $scope.sandwich + " from @thegoosewheel. What will you get?" };
       Tweet.sendTweet(order);
